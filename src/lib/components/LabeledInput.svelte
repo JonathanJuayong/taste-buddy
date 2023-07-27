ffd5e284bda149b9e7b308cb54027a7b00676318
Jonathan Juayong <jonathan.juayong@gmail.com>
2023-07-28 01:21:42 +1000

    feat: create component for form inputs

---
+++ src/lib/components/LabeledInput.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	export let name: string;
+	export let label: string = name;
+	export let error: string[] | undefined;
+	export let value: any;
+	export let type: 'text' | 'number';
+</script>
+
+<label>
+	<span class="ml-2 my-2 inline-block uppercase font-bold text-secondary-500 opacity-50 text-sm">
+		{label}
+	</span>
+	{#if type === 'text'}
+		<input
+			class="input"
+			{name}
+			type="text"
+			aria-invalid={error ? 'true' : undefined}
+			bind:value
+			{...$$restProps}
+		/>
+	{:else if type === 'number'}
+		<input
+			class="input"
+			{name}
+			type="number"
+			aria-invalid={error ? 'true' : undefined}
+			bind:value
+			{...$$restProps}
+		/>
+	{/if}
+</label>
+{#if error}<span class="invalid">{error}</span>{/if}
