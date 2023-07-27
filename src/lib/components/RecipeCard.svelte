8797f5f52b0e1eafe892fbd79a093289f21234ce
Jonathan Juayong <jonathan.juayong@gmail.com>
2023-07-27 23:45:37 +1000

    feat: create component for rendering recipe card

---
+++ src/lib/components/RecipeCard.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { RecipeCard } from '$lib/types';
+	import { CldImage } from 'svelte-cloudinary';
+
+	export let recipe: RecipeCard;
+	const { name, src, id } = recipe;
+	const effects = [{ opacity: 50 }, { blur: '500' }];
+</script>
+
+<a href="/recipes/{id}" class="card max-w-4xl relative overflow-hidden">
+	<CldImage height="" width="1000" alt={name} aspectRatio={1.0} {src} class="bg-black" {effects} />
+	<p class="absolute bottom-5 left-5 h2 font-bold">{name}</p>
+</a>
