47f44cac5e5cd18f9eac1e5cc472c4a6f3bd5634
Jonathan Juayong <jonathan.juayong@gmail.com>
2023-07-28 01:20:26 +1000

    feat: make the page name dynamic based on url

--- src/routes/app/+layout.svelte
+++ src/routes/app/+layout.svelte
@@ -1,10 +1,23 @@
 <script lang="ts">
-	// import type { LayoutData } from './$types';
 	import { AppBar } from '@skeletonlabs/skeleton';
 	import MenuIcon from '~icons/lucide/menu';
 	import BabyIcon from '~icons/lucide/baby';
+	import { page } from '$app/stores';
 
-	// export let data: LayoutData;
+	function getPageName(url: string) {
+		switch (url) {
+			case '/app': {
+				return 'Home';
+			}
+			case '/app/create': {
+				return 'Create';
+			}
+
+			default: {
+				return 'App';
+			}
+		}
+	}
 </script>
 
 <main class="flex flex-col h-full">
@@ -12,7 +25,7 @@
 		<svelte:fragment slot="lead"
 			><button class="btn btn-icon p-1"><MenuIcon /></button></svelte:fragment
 		>
-		<h1 class="h1">Home</h1>
+		<h1 class="h1">{getPageName($page.url.pathname)}</h1>
 		<svelte:fragment slot="trail"
 			><button class="btn btn-icon p-1"><BabyIcon /></button></svelte:fragment
 		>
