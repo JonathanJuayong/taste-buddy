a9a8923f676711122efe1fc33fe20dae0e767b52
Jonathan Juayong <jonathan.juayong@gmail.com>
2023-07-27 19:53:11 +1000

    feat: install and setup vite image tools

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import Icons from 'unplugin-icons/vite';
+import { imagetools } from 'vite-imagetools';
 
 export default defineConfig({
 	plugins: [
 		sveltekit(),
+		imagetools(),
 		Icons({
 			compiler: 'svelte',
 			autoInstall: true
