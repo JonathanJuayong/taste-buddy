f87b9e5a99828a95e37d7dbb17ec59ec8c0c735d
Jonathan Juayong <jonathan.juayong@gmail.com>
2023-07-27 19:25:43 +1000

    feat: create initial ui for home page with no recipes

---
+++ src/routes/app/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	// import type { PageData } from './$types';
+	import PlusIcon from '~icons/lucide/plus';
+
+	// export let data: PageData;
+</script>
+
+<article class="text-center space-y-4 self-center">
+	<p class="h2">You don't have any recipes yet</p>
+	<p>Click the button below to create your first recipe</p>
+	<button class="btn variant-filled-primary">Create New Recipe<PlusIcon class="ml-2" /></button>
+</article>
