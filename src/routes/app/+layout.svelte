8b2b062f42a78baf42137bb444e7171eee6d5ae7
Jonathan Juayong <jonathan.juayong@gmail.com>
2023-07-27 19:24:53 +1000

    feat: create initial layout for the app

---
+++ src/routes/app/+layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	// import type { LayoutData } from './$types';
+	import { AppBar } from '@skeletonlabs/skeleton';
+	import MenuIcon from '~icons/lucide/menu';
+	import BabyIcon from '~icons/lucide/baby';
+
+	// export let data: LayoutData;
+</script>
+
+<main class="flex flex-col h-full">
+	<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+		<svelte:fragment slot="lead"
+			><button class="btn btn-icon p-1"><MenuIcon /></button></svelte:fragment
+		>
+		<h1 class="h1">Home</h1>
+		<svelte:fragment slot="trail"
+			><button class="btn btn-icon p-1"><BabyIcon /></button></svelte:fragment
+		>
+	</AppBar>
+	<div class="flex-grow py-6 px-4 grid">
+		<slot class="flex-grow py-6 px-4 grid" />
+	</div>
+</main>
