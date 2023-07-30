6b82e0fb390df726b76d8e89826bb508e49716a7
Jonathan Juayong <jonathan.juayong@gmail.com>
2023-07-30 22:40:18 +1000

    feat: add ability to open drawer

--- src/routes/app/+layout.svelte
+++ src/routes/app/+layout.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
-	import { AppBar } from '@skeletonlabs/skeleton';
+	import { AppBar, drawerStore, type DrawerSettings } from '@skeletonlabs/skeleton';
 	import MenuIcon from '~icons/lucide/menu';
 	import BabyIcon from '~icons/lucide/baby';
 	import { page } from '$app/stores';
+
+	const drawerSettings: DrawerSettings = {
+		id: 'main',
+		position: 'left'
+	};
 
 	function getPageName(url: string) {
 		switch (url) {
@@ -18,17 +23,25 @@
 			}
 		}
 	}
+
+	function openDrawer() {
+		drawerStore.open(drawerSettings);
+	}
 </script>
 
 <main class="flex flex-col h-full">
 	<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
-		<svelte:fragment slot="lead"
-			><button class="btn btn-icon p-1"><MenuIcon /></button></svelte:fragment
-		>
+		<svelte:fragment slot="lead">
+			<button on:click={openDrawer} class="btn btn-icon p-1">
+				<MenuIcon />
+			</button>
+		</svelte:fragment>
 		<h1 class="h1">{getPageName($page.url.pathname)}</h1>
-		<svelte:fragment slot="trail"
-			><button class="btn btn-icon p-1"><BabyIcon /></button></svelte:fragment
-		>
+		<svelte:fragment slot="trail">
+			<button class="btn btn-icon p-1">
+				<BabyIcon />
+			</button>
+		</svelte:fragment>
 	</AppBar>
 	<div class="flex-grow py-6 px-4">
 		<slot />
