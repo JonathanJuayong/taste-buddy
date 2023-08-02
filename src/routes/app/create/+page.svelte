9a029995131306c31abf3415545b39f7a1153c0a
Jonathan Juayong <jonathan.juayong@gmail.com>
2023-08-02 13:37:26 +1000

    feat: add values to actionUrl and onResult props

--- src/routes/app/create/+page.svelte
+++ src/routes/app/create/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import RecipeForm from '$lib/components/RecipeForm.svelte';
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
 
 	export let data: PageData;
 </script>
 
-<RecipeForm formProp={data.form} />
+<RecipeForm formProp={data.form} actionUrl={$page.url.pathname} onResult={() => goto('/app')} />
