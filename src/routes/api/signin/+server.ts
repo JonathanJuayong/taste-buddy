b537cba16febd16bec9ab915653f6720cdd3e7db
Jonathan Juayong <jonathan.juayong@gmail.com>
2023-08-04 14:16:02 +1000

    feat: create api route for sign in and sign out using cookies

---
+++ src/routes/api/signin/+server.ts
@@ -0,0 +1,37 @@
+import { authAdmin } from '$lib/server/firebase-admin';
+import { error, json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+
+const maxAge = 60 * 60 * 24 * 5 * 1000;
+const cookieSessionKey = '__session';
+
+export const POST: RequestHandler = async ({ request, cookies }) => {
+	try {
+		const { idToken } = (await request.json()) as { idToken: string };
+		const { uid, auth_time } = await authAdmin.verifyIdToken(idToken);
+
+		const authIsLessThan5Mins = new Date().getTime() / 1000 - auth_time < 5 * 60;
+		if (authIsLessThan5Mins) {
+			const cookie = await authAdmin.createSessionCookie(idToken, { expiresIn: maxAge });
+			cookies.set(cookieSessionKey, cookie, {
+				maxAge,
+				httpOnly: true,
+				secure: true,
+				path: '/'
+			});
+
+			console.log('Cookie has been set');
+			return json({ status: 'signed in' });
+		} else {
+			throw error(401, 'Recent Sign-in Required');
+		}
+	} catch (e) {
+		console.log(e);
+		throw error(500, JSON.stringify(e));
+	}
+};
+
+export const DELETE: RequestHandler = async ({ cookies }) => {
+	cookies.delete(cookieSessionKey, { path: '/' });
+	return json({ status: 'signed out' });
+};
