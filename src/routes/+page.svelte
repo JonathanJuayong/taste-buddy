e85291aaee6751c896663fb7c9c10ca822a2397f
Jonathan Juayong <jonathan.juayong@gmail.com>
2023-08-05 00:03:06 +1000

    feat: remove boilerplate from home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,6 @@
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout, the CSS import order is critical!</li>
-			<li><code class="code">/src/app.postcss</code> - minimal css to make the page full screen, may not be relevant for your project</li>
-			<li><code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents</li>
-		</ul>
-	</div>
+<div class="flex gap-5 justify-center py-10">
+	<a class="btn variant-filled-primary" href="/signin">Sign In</a>
+	<a class="btn variant-filled-primary" href="/app">Go to App</a>
 </div>
