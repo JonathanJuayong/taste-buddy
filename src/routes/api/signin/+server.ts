3645a388a2880fedf6ccb1808c52530c73398ef3
Jonathan Juayong <jonathan.juayong@gmail.com>
2023-08-05 00:04:38 +1000

    feat: create user in db if not yet existing

--- src/routes/api/signin/+server.ts
+++ src/routes/api/signin/+server.ts
@@ -1,6 +1,7 @@
 import { authAdmin } from '$lib/server/firebase-admin';
 import { error, json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
+import { createUser, getUserById } from '$lib/server/db';
 
 const maxAge = 60 * 60 * 24 * 5 * 1000;
 const cookieSessionKey = '__session';
@@ -9,6 +10,11 @@ export const POST: RequestHandler = async ({ request, cookies }) => {
 	try {
 		const { idToken } = (await request.json()) as { idToken: string };
 		const { uid, auth_time } = await authAdmin.verifyIdToken(idToken);
+
+		const user = await getUserById(uid);
+		if (!user) {
+			await createUser(uid);
+		}
 
 		const authIsLessThan5Mins = new Date().getTime() / 1000 - auth_time < 5 * 60;
 		if (authIsLessThan5Mins) {
