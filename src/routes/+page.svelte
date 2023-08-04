7af41762e89b973c54e3f99324fb4a7e67c83b44
Jonathan Juayong <jonathan.juayong@gmail.com>
2023-08-05 00:05:17 +1000

    chore: remove comment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,3 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
-
 <div class="flex gap-5 justify-center py-10">
 	<a class="btn variant-filled-primary" href="/signin">Sign In</a>
 	<a class="btn variant-filled-primary" href="/app">Go to App</a>
