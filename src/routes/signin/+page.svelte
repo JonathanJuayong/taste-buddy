d7343e296528c9bff8d171732cafa8196b824133
Jonathan Juayong <jonathan.juayong@gmail.com>
2023-08-08 16:45:37 +1000

    feat: clean up sign in with provider and add email sign in

--- src/routes/signin/+page.svelte
+++ src/routes/signin/+page.svelte
@@ -1,29 +1,21 @@
 <script lang="ts">
 	import { auth } from '$lib/firebase';
-	import { signInWithPopup, type AuthProvider, GoogleAuthProvider, signOut } from 'firebase/auth';
+	import { signInWithEmailAndPassword, signOut } from 'firebase/auth';
 	import type { PageData } from './$types';
-	import GoogleIcon from '~icons/devicon-plain/google';
 	import { goto } from '$app/navigation';
+	import EmailIcon from '~icons/mdi/email-outline';
+	import SignInWithProviderButton from '$lib/components/SignInWithProviderButton.svelte';
+	import ButtonWithSpinner from '$lib/components/ButtonWithSpinner.svelte';
+	import { FirebaseError } from 'firebase/app';
 
 	export let data: PageData;
 	const uid = data.user.uid;
 
-	function signInWith(provider: AuthProvider) {
-		return async () => {
-			const { user } = await signInWithPopup(auth, provider);
-			const idToken = await user.getIdToken();
-			try {
-				await fetch('/api/signin', {
-					method: 'POST',
-					headers: { 'Content-Type': 'application/json' },
-					body: JSON.stringify({ idToken })
-				});
-				goto('/app');
-			} catch (error) {
-				console.log(error);
-			}
-		};
-	}
+	let email: string;
+	let password: string;
+	let isLoading = false;
+
+	let errorMessage: string | null = null;
 
 	async function logOut() {
 		await fetch('/api/signin', {
@@ -32,17 +24,71 @@
 		await signOut(auth);
 		goto('/');
 	}
+
+	async function signInWithEmail() {
+		errorMessage = null;
+		isLoading = true;
+		try {
+			const userCredential = await signInWithEmailAndPassword(auth, email, password);
+			const idToken = await userCredential.user.getIdToken();
+			await fetch('/api/signin', {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({ idToken })
+			});
+			await goto('/app');
+		} catch (error) {
+			isLoading = false;
+
+			if (error instanceof FirebaseError) {
+				console.log(error.code);
+				errorMessage = 'Email or password is incorrect. Please try again.';
+			}
+
+			console.log(error);
+		}
+	}
 </script>
 
 <div class="p-6 space-y-6">
 	{#if uid}
 		<button class="btn variant-filled-error w-full" on:click={logOut}>Sign Out</button>
 	{:else}
-		<button
-			class="btn variant-filled-primary w-full"
-			on:click={signInWith(new GoogleAuthProvider())}
+		<div>
+			<label>
+				<span
+					class="ml-2 my-2 inline-block uppercase font-bold text-secondary-500 opacity-50 text-sm"
+				>
+					Email
+				</span>
+				<input class="input" bind:value={email} />
+			</label>
+			<label>
+				<span
+					class="ml-2 my-2 inline-block uppercase font-bold text-secondary-500 opacity-50 text-sm"
+				>
+					Password
+				</span>
+				<input type="password" class="input" bind:value={password} />
+			</label>
+		</div>
+		{#if errorMessage}
+			<p class="text-error-900">{errorMessage}</p>
+		{/if}
+		<ButtonWithSpinner
+			variantDefault="variant-filled-secondary"
+			variantLoading="variant-outline-secondary"
+			{isLoading}
+			on:click={signInWithEmail}
 		>
-			Sign In With Google <GoogleIcon class="ml-2" />
-		</button>
+			Sign In With Email
+			<svelte:fragment slot="button-icon">
+				<EmailIcon class="ml-2" />
+			</svelte:fragment>
+		</ButtonWithSpinner>
+		<div class="space-y-6 border-t-[1px] border-surface-500 pt-6">
+			<SignInWithProviderButton providerName="google" />
+			<SignInWithProviderButton providerName="microsoft" />
+		</div>
 	{/if}
 </div>
