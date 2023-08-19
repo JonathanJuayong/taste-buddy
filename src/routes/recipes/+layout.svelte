685322ec06e783c089e2a4a7d5add0217235d317
Jonathan Juayong <jonathan.juayong@gmail.com>
2023-08-19 16:56:35 +1000

    feat: add breadcrumb to page layout

---
+++ src/routes/recipes/+layout.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import HomeIcon from '~icons/lucide/home';
+</script>
+
+<article class="m-4">
+	<ul class="breadcrumb-nonresponsive mb-6">
+		{#if $page.url.pathname === '/recipes'}
+			<li>
+				<p class="flex items-center">
+					<HomeIcon class="mr-2" />
+					Recipes
+				</p>
+			</li>
+		{:else}
+			<li class="crumb">
+				<a href="/recipes" class="anchor flex items-center">
+					<HomeIcon class="mr-2" />
+					Recipes
+				</a>
+			</li>
+		{/if}
+		{#if $page.params?.id}
+			<li class="crumb-separator">
+				{'>'}
+			</li>
+			<li class="crumb">
+				<p>{$page.params?.id}</p>
+			</li>
+		{/if}
+		<li />
+	</ul>
+	<slot />
+</article>
