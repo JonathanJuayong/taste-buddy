edde872bb81101b51622785e1d8f94caa64ad6a1
Jonathan Juayong <jonathan.juayong@gmail.com>
2023-08-19 16:54:09 +1000

    feat: add ability to "save" the state of browsing by user and also remove checking of isLastPage

--- src/routes/recipes/+page.server.ts
+++ src/routes/recipes/+page.server.ts
@@ -1,26 +1,32 @@
+import type { MainSchema } from '$lib/formSchema';
 import { searchRecipesByNamePaginated } from '$lib/server/db';
 import type { PageServerLoad } from './$types';
 
 const resultsPerPage = 5;
 const initialId = 1_000_000;
+const maxNumberOfItemsToBeFetched = 100;
 
 export const load = (async ({ url }) => {
-	const search = url.searchParams.get('search');
+	const params = url.searchParams;
+	const search = params.get('search');
+	const items = params.get('items') as number | null;
+	const id = params.get('id') as number | null;
 
-	// try to look 1 row ahead to check if data returned is the last batch of data,
-	// hence the + 1 on resultsPerPage
-	const recipes = await searchRecipesByNamePaginated(search ?? '', initialId, resultsPerPage + 1);
+	const numberOfItemsToBeFetched = items ?? resultsPerPage;
 
-	// disregard the extra row fetched as it is only used to check if there are still data to be fetched
-	const response = recipes.slice(0, resultsPerPage);
-	const lastSeenId = response.at(-1)?.id ?? initialId;
+	// only fetch upto max number of items in db. ignore value if it exceeds max limit
+	const limitedItems =
+		numberOfItemsToBeFetched > maxNumberOfItemsToBeFetched
+			? maxNumberOfItemsToBeFetched
+			: numberOfItemsToBeFetched;
 
-	// if recipes.length is the same length or less than resultsPerPage despite fetching an additional row,
-	// then we can confirm that there are no more recipes to be fetched and we can tell the
-	// frontend to not make any more calls to db to fetch more data
-	if (recipes.length <= resultsPerPage) {
-		return { recipes: response, resultsPerPage, isLastPage: true, lastSeenId };
-	}
+	const recipes: MainSchema[] = await searchRecipesByNamePaginated(
+		search ?? '',
+		id ?? initialId,
+		limitedItems
+	);
 
-	return { recipes: response, resultsPerPage, isLastPage: false, lastSeenId };
+	const lastSeenId = recipes.at(-1)?.id ?? initialId;
+
+	return { recipes, resultsPerPage, isLastPage: false, lastSeenId };
 }) satisfies PageServerLoad;
