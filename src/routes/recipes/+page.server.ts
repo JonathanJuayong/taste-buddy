0f4ab5f715553c05fc52a41559405aedd9f6843b
Jonathan Juayong <jonathan.juayong@gmail.com>
2023-08-19 15:06:14 +1000

    feat: reimplement load function to be able to fetch recipes via search and remove form action

--- src/routes/recipes/+page.server.ts
+++ src/routes/recipes/+page.server.ts
@@ -1,45 +1,26 @@
-import { getRecipesPaginated } from '$lib/server/db';
+import { searchRecipesByNamePaginated } from '$lib/server/db';
 import type { PageServerLoad } from './$types';
 
 const resultsPerPage = 5;
+const initialId = 1_000_000;
 
-export const load = (async () => {
-	const initialId = 1_000_000;
+export const load = (async ({ url }) => {
+	const search = url.searchParams.get('search');
 
 	// try to look 1 row ahead to check if data returned is the last batch of data,
 	// hence the + 1 on resultsPerPage
-	const recipes = await getRecipesPaginated(resultsPerPage + 1, initialId);
+	const recipes = await searchRecipesByNamePaginated(search ?? '', initialId, resultsPerPage + 1);
 
 	// disregard the extra row fetched as it is only used to check if there are still data to be fetched
 	const response = recipes.slice(0, resultsPerPage);
+	const lastSeenId = response.at(-1)?.id ?? initialId;
 
 	// if recipes.length is the same length or less than resultsPerPage despite fetching an additional row,
 	// then we can confirm that there are no more recipes to be fetched and we can tell the
 	// frontend to not make any more calls to db to fetch more data
 	if (recipes.length <= resultsPerPage) {
-		return { recipes: response, resultsPerPage, isLastPage: true };
+		return { recipes: response, resultsPerPage, isLastPage: true, lastSeenId };
 	}
 
-	return { recipes: response, resultsPerPage, isLastPage: false };
+	return { recipes: response, resultsPerPage, isLastPage: false, lastSeenId };
 }) satisfies PageServerLoad;
-
-export const actions = {
-	load: async ({ request }) => {
-		const result = await request.formData();
-		const value = result.get('value') as number | null;
-
-		if (!value) return;
-
-		// do the same thing as in the load function
-		const recipes = await getRecipesPaginated(resultsPerPage + 1, value);
-		const response = recipes.slice(0, resultsPerPage);
-
-		if (recipes.length <= resultsPerPage)
-			return {
-				recipes: response,
-				isLastPage: true
-			};
-
-		return { recipes: response, isLastPage: false };
-	}
-};
