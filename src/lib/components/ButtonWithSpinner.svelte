cb9b20accef251a726cf778642394da05153010c
Jonathan Juayong <jonathan.juayong@gmail.com>
2023-08-21 02:34:21 +1000

    feat: replace conic gradient with progress radial

--- src/lib/components/ButtonWithSpinner.svelte
+++ src/lib/components/ButtonWithSpinner.svelte
@@ -1,20 +1,21 @@
 <script lang="ts">
-	import { ConicGradient, type ConicStop } from '@skeletonlabs/skeleton';
+	import { ProgressRadial } from '@skeletonlabs/skeleton';
 
 	export let isLoading: boolean;
 	export let variantLoading: string = 'variant-outline-primary';
 	export let variantDefault: string = 'variant-filled-primary';
-
-	const conicStops: ConicStop[] = [
-		{ color: 'transparent', start: 0, end: 25 },
-		{ color: 'rgb(var(--color-primary-500))', start: 75, end: 100 }
-	];
 </script>
 
 {#if isLoading}
 	<button class="btn variant-outline-primary w-full {variantLoading}" disabled>
 		<slot />
-		<ConicGradient stops={conicStops} class="ml-2" width="w-6" spin />
+		<ProgressRadial
+			stroke={120}
+			class="ml-4"
+			width="w-6"
+			meter="stroke-primary-500"
+			track="stroke-primary-500/30"
+		/>
 	</button>
 {:else}
 	<button class="btn w-full {variantDefault}" on:click>
