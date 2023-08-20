5a94940279b97ed588d103a3d0c80f5694c7b7a3
Jonathan Juayong <jonathan.juayong@gmail.com>
2023-08-21 02:15:06 +1000

    feat: update layout to separate empty state and populated state

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -7,24 +7,24 @@
 	const { recipes } = data;
 </script>
 
-<div class="grid sm:grid-cols-2 gap-6 items-center h-full">
-	{#if recipes.length === 0}
-		<article class="text-center space-y-4">
-			<p class="h2">You don't have any recipes yet</p>
-			<p>Click the button below to create your first recipe</p>
-			<a href="/app/create" class="btn variant-filled-primary"
-				>Create New Recipe<PlusIcon class="ml-2" /></a
-			>
-		</article>
-	{:else}
+{#if recipes.length === 0}
+	<div class="text-center py-20 space-y-4">
+		<p class="h2">You don't have any recipes yet</p>
+		<p>Click the button below to create your first recipe</p>
+		<a href="/app/create" class="btn variant-filled-primary"
+			>Create New Recipe<PlusIcon class="ml-2" /></a
+		>
+	</div>
+{:else}
+	<div class="grid sm:grid-cols-2 gap-6 items-center h-full">
 		{#each recipes as recipe}
 			<RecipeCard {recipe} />
 		{/each}
-	{/if}
-</div>
+	</div>
+{/if}
 <a
 	href="/app/create"
-	class="btn aspect-square rounded-full variant-filled-primary fixed bottom-8 right-8"
+	class="btn aspect-square rounded-full variant-filled-primary fixed bottom-8 right-8 sm:hidden"
 >
 	<PlusIcon />
 </a>
