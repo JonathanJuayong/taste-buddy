542c96e87ede4c3bd3a994690a4af407f4adfb24
Jonathan Juayong <jonathan.juayong@gmail.com>
2023-08-21 02:36:27 +1000

    feat: temporarily redirect to recipes when navigating to home page

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,9 @@
+import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load = (async ({ locals }) => {
+	throw redirect(301, '/recipes');
+
 	const user = locals.user ?? null;
 	return { user };
 }) satisfies PageServerLoad;
