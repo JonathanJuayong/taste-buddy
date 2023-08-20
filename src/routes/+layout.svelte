317c44a09ef5bca2815409a451cecadd180ff26c
Jonathan Juayong <jonathan.juayong@gmail.com>
2023-08-21 00:57:38 +1000

    feat: create sitewide app bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,8 +5,25 @@
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
-	import { Drawer, Toast, drawerStore } from '@skeletonlabs/skeleton';
+	import { AppBar, Drawer, Toast, drawerStore, type DrawerSettings } from '@skeletonlabs/skeleton';
 	import XIcon from '~icons/lucide/x';
+	import MenuIcon from '~icons/lucide/menu';
+	import type { LayoutData } from './$types';
+
+	export let data: LayoutData;
+
+	$: ({
+		user: { uid }
+	} = data);
+
+	const drawerSettings: DrawerSettings = {
+		id: 'main',
+		position: 'left'
+	};
+
+	function openDrawer() {
+		drawerStore.open(drawerSettings);
+	}
 </script>
 
 <Toast />
@@ -18,18 +35,51 @@
 	}}
 >
 	<ul class="space-y-5">
-		<li><a class="text-2xl" href="/app">Home</a></li>
-		<li><a class="text-2xl" href="/app/create">Create</a></li>
+		<li><a class="text-2xl" href="/app">App</a></li>
+		<li><a class="text-2xl" href="/recipes">Recipes</a></li>
+		<li><a class="text-2xl" href="/signin">Sign in</a></li>
+		{#if uid}
+			<li><a class="text-2xl" href="/app/create">Create</a></li>
+		{/if}
 	</ul>
 	<button class="btn absolute top-4 right-2" on:click={() => drawerStore.close()}><XIcon /></button>
 </Drawer>
 
-<nav class="flex items-center justify-between py-5">
+<AppBar
+	class="sm:hidden mb-8"
+	gridColumns="grid-cols-3"
+	slotDefault="place-self-center"
+	slotTrail="place-content-end"
+>
+	<svelte:fragment slot="lead">
+		<button on:click={openDrawer} class="btn btn-icon p-1">
+			<MenuIcon />
+		</button>
+	</svelte:fragment>
 	<p class="font-extrabold">Taste Buddy</p>
-	<ul class="flex gap-6">
-		<li><a href="/app">App</a></li>
-		<li><a href="/recipes">Recipes</a></li>
-		<li><a href="signin">Sign in</a></li>
-	</ul>
-</nav>
-<slot />
+</AppBar>
+
+<div class="px-6">
+	<nav class="sm:flex items-center justify-between py-4 mb-6 hidden">
+		<p class="font-extrabold">Taste Buddy</p>
+		<ul class="flex gap-6 items-center">
+			<li><a href="/app">App</a></li>
+			<li><a href="/recipes">Recipes</a></li>
+			<li>
+				<a href="signin">
+					{#if uid}
+						Sign out
+					{:else}
+						Sign in
+					{/if}
+				</a>
+			</li>
+			{#if uid}
+				<li class="variant-outline-primary btn btn-sm">
+					<a class="text-primary-700" href="/app/create">Create</a>
+				</li>
+			{/if}
+		</ul>
+	</nav>
+	<slot />
+</div>
