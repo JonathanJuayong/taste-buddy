cfe271a5c9301c3b7dcda9498865fa70103442dd
Jonathan Juayong <jonathan.juayong@gmail.com>
2023-08-22 19:56:06 +1000

    chore: remove unused stores

--- src/stores/recipes.ts
+++
@@ -1,6 +0,0 @@
-import type { MainSchema } from '$lib/formSchema';
-import { localStorageStore } from '@skeletonlabs/skeleton';
-
-export const recipes = localStorageStore<MainSchema[]>('recipes', []);
-
-export const scrollStore = localStorageStore<number>('scroll', 0);
