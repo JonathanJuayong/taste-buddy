69895a1aed1240db8e35f929ec230e7ebca190f3
Jonathan Juayong <jonathan.juayong@gmail.com>
2023-08-22 23:46:38 +1000

    feat: confirm with user first before deleting recipe

--- src/routes/app/edit/[id]/+page.svelte
+++ src/routes/app/edit/[id]/+page.svelte
@@ -3,6 +3,9 @@
 	import { page } from '$app/stores';
 	import RecipeForm from '$lib/components/RecipeForm.svelte';
 	import { goto } from '$app/navigation';
+	import { enhance } from '$app/forms';
+	import { toastStore } from '@skeletonlabs/skeleton';
+	import { confirmModal } from '$lib/utils';
 
 	export let data: PageData;
 	const { form, editable } = data;
@@ -18,7 +21,27 @@
 			formProp={form}
 			onResult={async () => await goto('/app')}
 		/>
-		<form method="POST" action="{$page.url.pathname}?/delete">
+		<form
+			method="POST"
+			action="{$page.url.pathname}?/delete"
+			use:enhance={async ({ cancel }) => {
+				const response = await confirmModal(
+					`Delete Recipe: ${recipe?.name}`,
+					'Are you sure you want to delete this recipe?'
+				);
+				if (response) {
+					return async ({ update }) => {
+						await update();
+						toastStore.trigger({
+							message: `${recipe?.name} has been successfully deleted`,
+							background: 'variant-filled-primary'
+						});
+					};
+				} else {
+					cancel();
+				}
+			}}
+		>
 			<button class="btn variant-outline-error w-full"> Delete Recipe </button>
 		</form>
 		<a href="/app" class="btn variant-outline-tertiary"> Cancel Edit </a>
