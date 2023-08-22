5197771dd36009370a53a6dca7a0dcbe56bcebaa
Jonathan Juayong <jonathan.juayong@gmail.com>
2023-08-22 18:48:53 +1000

    feat: add loading state when fetching data

--- src/routes/recipes/+page.svelte
+++ src/routes/recipes/+page.svelte
@@ -5,12 +5,15 @@
 	import SearchIcon from '~icons/lucide/search';
 	import { page } from '$app/stores';
 	import type { MainSchema } from '$lib/formSchema';
+	import { ProgressRadial, toastStore } from '@skeletonlabs/skeleton';
 
 	export let data: PageData;
 
 	$: ({ recipes, lastSeenId, resultsPerPage, isLastPage } = data);
 
 	let searchQuery: string;
+
+	let isLoading = false;
 
 	$: idForFetchingNewRecipes = lastSeenId;
 	$: continueFetching = !isLastPage;
@@ -24,6 +27,7 @@
 		  }`;
 
 	function fetchMoreRecipes(searchQuery: string, lastSeenId: number, resultsPerPage: number) {
+		isLoading = true;
 		return async () => {
 			const body = {
 				searchQuery: searchQuery ?? '',
@@ -31,23 +35,34 @@
 				resultsPerPage
 			};
 
-			const request = await fetch('/api/recipes', {
-				method: 'post',
-				headers: { 'Content-Type': 'application/json' },
-				body: JSON.stringify(body)
-			});
+			try {
+				const request = await fetch('/api/recipes', {
+					method: 'post',
+					headers: { 'Content-Type': 'application/json' },
+					body: JSON.stringify(body)
+				});
 
-			const data = (await request.json()) as {
-				recipes: MainSchema[];
-				isLastPage: boolean;
-				lastId: number;
-			};
+				const data = (await request.json()) as {
+					recipes: MainSchema[];
+					isLastPage: boolean;
+					lastId: number;
+				};
 
-			continueFetching = !data.isLastPage;
-			idForFetchingNewRecipes = data.lastId;
-			recipesFetched = [...recipesFetched, ...data.recipes];
+				isLoading = false;
 
-			window.history.pushState({ path: $page.url.pathname }, '', href);
+				continueFetching = !data.isLastPage;
+				idForFetchingNewRecipes = data.lastId;
+				recipesFetched = [...recipesFetched, ...data.recipes];
+
+				window.history.pushState({ path: $page.url.pathname }, '', href);
+			} catch (error) {
+				console.log(error);
+				toastStore.trigger({
+					message: 'Oops... something went wrong. Try again.',
+					background: 'variant-filled-warning'
+				});
+				isLoading = false;
+			}
 		};
 	}
 </script>
@@ -88,12 +103,22 @@
 </ul>
 
 {#if continueFetching}
-	<button
-		class="btn mx-auto block my-16 variant-soft-secondary"
-		on:click={fetchMoreRecipes(searchQuery, idForFetchingNewRecipes, resultsPerPage)}
-	>
-		Fetch More Recipes
-	</button>
+	{#if isLoading}
+		<ProgressRadial
+			stroke={120}
+			class="mx-auto my-20"
+			width="w-16"
+			meter="stroke-primary-500"
+			track="stroke-primary-500/30"
+		/>
+	{:else}
+		<button
+			class="btn mx-auto block my-16 variant-soft-secondary"
+			on:click={fetchMoreRecipes(searchQuery, idForFetchingNewRecipes, resultsPerPage)}
+		>
+			Fetch More Recipes
+		</button>
+	{/if}
 {:else}
 	<p class="text-center mx-auto my-16">No more recipes to fetch</p>
 {/if}
