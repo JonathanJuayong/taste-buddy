31e0bc17b9c492100de42d26531abad45e52573b
Jonathan Juayong <jonathan.juayong@gmail.com>
2023-08-23 18:36:59 +1000

    feat: handle uploading and deleting of previous image in form action

--- src/routes/app/edit/[id]/+page.server.ts
+++ src/routes/app/edit/[id]/+page.server.ts
@@ -1,8 +1,8 @@
 import { deleteRecipe, getRecipeById, updateRecipe } from '$lib/server/db';
 import { superValidate } from 'sveltekit-superforms/client';
 import type { PageServerLoad } from './$types';
-import { mainSchema } from '$lib/formSchema';
-import { redirect } from '@sveltejs/kit';
+import { mainSchema, type MainSchema } from '$lib/formSchema';
+import { error, redirect } from '@sveltejs/kit';
 import { createTemporaryRedirectCookie } from '$lib/server/helpers';
 
 export const load = (async ({ params, locals, cookies }) => {
@@ -30,10 +30,37 @@ export const load = (async ({ params, locals, cookies }) => {
 }) satisfies PageServerLoad;
 
 export const actions = {
-	update: async ({ request }) => {
-		const form = await superValidate(request, mainSchema);
-		const newRecipe = form.data;
-		updateRecipe(Number(newRecipe.id), newRecipe);
+	update: async ({ request, fetch }) => {
+		try {
+			const data = await request.clone().formData();
+			const file = data.get('file') as File | null;
+			const previousImage = data.get('previousImage') as string | null;
+
+			const form = await superValidate(request.clone(), mainSchema);
+
+			if (file) {
+				const formData = new FormData();
+				formData.append('file', file);
+				formData.append('publicId', previousImage ?? '');
+
+				// delete previous image
+				await fetch('/api/images', { method: 'DELETE', body: formData });
+
+				// upload new image
+				const req = await fetch('/api/images', { method: 'POST', body: formData });
+				const { public_id } = (await req.json()) as { public_id: string };
+
+				// replace old with new image in db
+				const newRecipe: MainSchema = { ...form.data, image_src: public_id ?? form.data.image_src };
+				updateRecipe(Number(newRecipe.id), newRecipe);
+			} else {
+				const newRecipe = form.data;
+				updateRecipe(Number(newRecipe.id), newRecipe);
+			}
+		} catch (e) {
+			console.log(e);
+			throw error(400, 'Shit hit the fan');
+		}
 	},
 	delete: async ({ params }) => {
 		const { id } = params;
