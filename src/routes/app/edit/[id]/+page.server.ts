0fe69b728994f8a3558ba3311a1f85ce6475dc13
Jonathan Juayong <jonathan.juayong@gmail.com>
2023-08-25 01:50:35 +1000

    refactor: remove form action for deleting recipes

--- src/routes/app/edit/[id]/+page.server.ts
+++ src/routes/app/edit/[id]/+page.server.ts
@@ -61,18 +61,5 @@ export const actions = {
 			console.log(e);
 			throw error(400, 'Shit hit the fan');
 		}
-	},
-	delete: async ({ params, request, fetch }) => {
-		const { id } = params;
-		deleteRecipe(Number(id));
-		const formData = await request.formData();
-
-		try {
-			await fetch('/api/images', { method: 'DELETE', body: formData });
-		} catch (e) {
-			console.log(e);
-		}
-
-		throw redirect(302, '/app');
 	}
 };
