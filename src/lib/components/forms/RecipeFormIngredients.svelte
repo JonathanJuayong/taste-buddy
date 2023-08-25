7e01cb42cbf1645c5ca2fbfa59903a906af800c0
Jonathan Juayong <jonathan.juayong@gmail.com>
2023-08-26 03:22:18 +1000

    feat: add container with fixed height to contain height of element

--- src/lib/components/forms/RecipeFormIngredients.svelte
+++ src/lib/components/forms/RecipeFormIngredients.svelte
@@ -22,38 +22,40 @@
 <section class="grid gap-4">
 	<h2 class="h2">Ingredients</h2>
 	<p>List down all the ingredients needed for your recipe</p>
-	{#each $form.ingredients as _, i}
-		<div class="grid grid-cols-4 gap-6 relative" use:focus={i}>
-			<LabeledInput
-				name="qty"
-				type="number"
-				error={$errors.ingredients?.[i].qty}
-				bind:value={$form.ingredients[i].qty}
-				{...$constraints.ingredients?.qty}
-			/>
-			<LabeledInput
-				name="unit"
-				type="text"
-				error={$errors.ingredients?.[i].unit}
-				bind:value={$form.ingredients[i].unit}
-				{...$constraints.ingredients?.unit}
-			/>
-			<div class="col-span-2">
+	<div class="max-h-[30em] overflow-y-scroll">
+		{#each $form.ingredients as _, i}
+			<div class="grid grid-cols-4 gap-6 relative" use:focus={i}>
 				<LabeledInput
-					name="item"
-					type="text"
-					error={$errors.ingredients?.[i].item}
-					bind:value={$form.ingredients[i].item}
-					{...$constraints.ingredients?.item}
+					name="qty"
+					type="number"
+					error={$errors.ingredients?.[i].qty}
+					bind:value={$form.ingredients[i].qty}
+					{...$constraints.ingredients?.qty}
 				/>
+				<LabeledInput
+					name="unit"
+					type="text"
+					error={$errors.ingredients?.[i].unit}
+					bind:value={$form.ingredients[i].unit}
+					{...$constraints.ingredients?.unit}
+				/>
+				<div class="col-span-2">
+					<LabeledInput
+						name="item"
+						type="text"
+						error={$errors.ingredients?.[i].item}
+						bind:value={$form.ingredients[i].item}
+						{...$constraints.ingredients?.item}
+					/>
+				</div>
+				{#if deleteIngredientBtnIsShown}
+					<button class="absolute top-2 right-2" type="button" on:click={removeIngredient(i)}>
+						<XIcon class="text-error-900" />
+					</button>
+				{/if}
 			</div>
-			{#if deleteIngredientBtnIsShown}
-				<button class="absolute top-2 right-2" type="button" on:click={removeIngredient(i)}>
-					<XIcon class="text-error-900" />
-				</button>
-			{/if}
-		</div>
-	{/each}
+		{/each}
+	</div>
 	<button class="btn variant-outline-primary mt-8 w-full" type="button" on:click={addIngredient}>
 		Add
 	</button>
--- src/lib/components/forms/RecipeFormSteps.svelte
+++ src/lib/components/forms/RecipeFormSteps.svelte
@@ -25,36 +25,38 @@
 		List down all the steps needed to create your recipe. Try to be as descriptive as possible. You
 		can rearrange the order of the steps by dragging the handle at the left
 	</p>
-	<SortableList
-		itemListWithId={$form.steps}
-		onDragEnd={({ offsetY, indexOffset, i }) => {
-			if (offsetY > 0) {
-				$form.steps = moveItemTo($form.steps, i, i + indexOffset);
-			} else {
-				$form.steps = moveItemTo($form.steps, i, i - indexOffset);
-			}
-		}}
-		let:i
-	>
-		<div class="flex items-end gap-2" use:focus={i}>
-			<HandleIcon class="mb-3 handle cursor-move" />
-			<div class="flex-1">
-				<LabeledInput
-					name="steps"
-					label={`Step #${i + 1}`}
-					type="text"
-					error={$errors.steps?.[i].step}
-					bind:value={$form.steps[i].step}
-					{...$constraints.steps}
-				/>
+	<div class="max-h-[30em] overflow-y-scroll">
+		<SortableList
+			itemListWithId={$form.steps}
+			onDragEnd={({ offsetY, indexOffset, i }) => {
+				if (offsetY > 0) {
+					$form.steps = moveItemTo($form.steps, i, i + indexOffset);
+				} else {
+					$form.steps = moveItemTo($form.steps, i, i - indexOffset);
+				}
+			}}
+			let:i
+		>
+			<div class="flex items-end gap-2">
+				<HandleIcon class="mb-3 handle cursor-move" />
+				<div class="flex-1" use:focus={i}>
+					<LabeledInput
+						name="steps"
+						label={`Step #${i + 1}`}
+						type="text"
+						error={$errors.steps?.[i].step}
+						bind:value={$form.steps[i].step}
+						{...$constraints.steps}
+					/>
+				</div>
+				{#if deleteStepBtnIsShown}
+					<button class="absolute top-2 right-2" type="button" on:click={removeStep(i)}>
+						<XIcon class="text-error-900" />
+					</button>
+				{/if}
 			</div>
-			{#if deleteStepBtnIsShown}
-				<button class="absolute top-2 right-2" type="button" on:click={removeStep(i)}>
-					<XIcon class="text-error-900" />
-				</button>
-			{/if}
-		</div>
-	</SortableList>
+		</SortableList>
+	</div>
 	<button class="btn variant-outline-primary mt-8 w-full" type="button" on:click={addStep}>
 		Add
 	</button>
