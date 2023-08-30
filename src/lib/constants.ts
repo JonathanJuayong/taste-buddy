9e0369ce183a1dcc8fd05b9d08635141e5e394db
Jonathan Juayong <jonathan.juayong@gmail.com>
2023-08-30 11:37:21 +1000

    feat: create constants file

---
+++ src/lib/constants.ts
@@ -0,0 +1,23 @@
+export const MIN_INGREDIENT_QTY = 1;
+export const MAX_INGREDIENT_QTY = 10_000;
+export const MIN_INGREDIENT_UNIT_LENGTH = 1;
+export const MAX_INGREDIENT_UNIT_LENGTH = 10_000;
+export const MIN_INGREDIENT_ITEM_LENGTH = 1;
+export const MAX_INGREDIENT_ITEM_LENGTH = 10_000;
+
+export const MIN_STEP_LENGTH = 5;
+export const MAX_STEP_LENGTH = 1_000;
+
+export const MIN_LENGTH_NAME = 1;
+export const MAX_LENGTH_NAME = 150;
+export const MIN_LENGTH_DESCRIPTION = 1;
+export const MAX_LENGTH_DESCRIPTION = 300;
+export const MIN_NUMBER_SERVES = 1;
+export const MAX_NUMBER_SERVES = 1_000;
+export const MIN_NUMBER_COOK_TIME = 1;
+export const MAX_NUMBER_COOK_TIME = 1_000;
+export const MIN_NUMBER_PREP_TIME = 1;
+export const MAX_NUMBER_PREP_TIME = 1_000;
+
+export const MAX_NUMBER_INGREDIENTS = 5;
+export const MAX_NUMBER_STEPS = 5;
