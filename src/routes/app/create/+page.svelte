525a6768b5f454f71a68ce7417eece98c2227d3f
Jonathan Juayong <jonathan.juayong@gmail.com>
2023-08-30 15:07:31 +1000

    feat: let users know that recipe creation failed because there is no image

--- src/routes/app/create/+page.svelte
+++ src/routes/app/create/+page.svelte
@@ -1,12 +1,20 @@
 <script lang="ts">
-	import type { PageData } from './$types';
+	import type { ActionData, PageData } from './$types';
 	import RecipeForm from '$lib/components/forms/RecipeForm.svelte';
-	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
-	import { applyAction } from '$app/forms';
 	import { toastStore } from '@skeletonlabs/skeleton';
 
 	export let data: PageData;
+	export let form: ActionData;
+
+	$: {
+		if (form?.error) {
+			toastStore.trigger({
+				message: form.error,
+				background: 'variant-filled-error'
+			});
+		}
+	}
 </script>
 
 <RecipeForm formProp={data.form} actionUrl={$page.url.pathname} redirectUrl="/app" />
