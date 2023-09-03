c6733ceec5637dc0cdfd64b8dc14df4d26c68cbf
Jonathan Juayong <jonathan.juayong@gmail.com>
2023-09-04 03:10:15 +1000

    feat: implement ability to update profile details

--- src/routes/app/profile/+page.server.ts
+++ src/routes/app/profile/+page.server.ts
@@ -1,5 +1,5 @@
-import { getUserById } from '$lib/server/db';
-import { redirect } from '@sveltejs/kit';
+import { getUserById, updateUser } from '$lib/server/db';
+import { error, redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import { createTemporaryRedirectCookie } from '$lib/server/helpers';
 import { superValidate } from 'sveltekit-superforms/client';
@@ -22,7 +22,50 @@ export const load = (async ({ locals, cookies }) => {
 }) satisfies PageServerLoad;
 
 export const actions = {
-	async default({ request }) {
+	async default({ request, locals, fetch }) {
 		const form = await superValidate(request.clone(), userSchema);
+		const {
+			data: { bio, name, profile_picture }
+		} = form;
+
+		const {
+			user: { uid }
+		} = locals;
+
+		if (!uid) {
+			throw error(400, 'uid cannot be null or undefined');
+		}
+
+		try {
+			const data = await request.clone().formData();
+			const file = data.get('file') as File | null;
+			const previousImage = data.get('previousImage') as string | null;
+
+			if (!form.valid) {
+				throw error(400, 'Invalid data submitted');
+			}
+
+			if (file && file.size > 0) {
+				const formData = new FormData();
+				formData.append('file', file);
+				formData.append('publicId', previousImage ?? '');
+
+				// delete previous image
+				await fetch('/api/images', { method: 'DELETE', body: formData });
+
+				// upload new image
+				const req = await fetch('/api/images', { method: 'POST', body: formData });
+				const { public_id } = (await req.json()) as { public_id: string };
+
+				// replace old with new image in db
+				await updateUser(uid, name, bio, public_id);
+			} else {
+				await updateUser(uid, name, bio, profile_picture);
+			}
+		} catch (e) {
+			throw error(400, `Something went wrong: ${e}`);
+		}
+
+		return { form };
 	}
 };
--- src/routes/app/profile/+page.svelte
+++ src/routes/app/profile/+page.svelte
@@ -3,13 +3,63 @@
 	import type { PageData } from './$types';
 	import LabeledInput from '$lib/components/LabeledInput.svelte';
 	import UploadWidget from '$lib/components/UploadWidget.svelte';
+	import { toastStore } from '@skeletonlabs/skeleton';
+	import { goto } from '$app/navigation';
+	import ButtonWithSpinner from '$lib/components/ButtonWithSpinner.svelte';
+	import { page } from '$app/stores';
 
 	export let data: PageData;
+	let imageFile: File | null;
+	let previousImage: string | null;
+	let isLoading = false;
 
-	const { form, errors, constraints, enhance } = superForm(data.form, {});
+	const { form, errors, constraints, enhance } = superForm(data.form, {
+		onSubmit({ formData }) {
+			isLoading = true;
+			formData.append('profile_picture', $form.profile_picture);
+			if (imageFile) {
+				formData.append('file', imageFile);
+				formData.append('previousImage', previousImage ?? '');
+			}
+		},
+		onResult: async (data) => {
+			const {
+				result: { type }
+			} = data;
+			isLoading = false;
+			if (type === 'failure') {
+				toastStore.trigger({
+					message:
+						'Something went wrong when submitting your data. Please double check the information submitted and try again.',
+					background: 'variant-filled-error'
+				});
+			}
+
+			if (type === 'success') {
+				toastStore.trigger({
+					message: 'Data was submitted successfully!'
+				});
+				await goto('/app');
+			}
+		},
+		onError: () => {
+			toastStore.trigger({
+				message:
+					'There was an error with the data submitted for this recipe. Please double check the information submitted and try again.',
+				background: 'variant-filled-error'
+			});
+			isLoading = false;
+		}
+	});
 </script>
 
-<form method="post" class="mb-20 space-y-10" use:enhance>
+<form
+	enctype="multipart/form-data"
+	action={$page.url.pathname}
+	method="post"
+	class="mb-20 space-y-10"
+	use:enhance
+>
 	<section class="space-y-4">
 		<h2 class="h2">Update Profile</h2>
 		<p>
@@ -19,7 +69,16 @@
 		</p>
 	</section>
 	<div>
-		<UploadWidget rounded aspectRatio={1} width={400} imageSrc={$form.profile_picture} />
+		<UploadWidget
+			rounded
+			aspectRatio={1}
+			width={400}
+			imageSrc={$form.profile_picture}
+			on:imageChange={(e) => {
+				imageFile = e.detail;
+				previousImage = $form.profile_picture;
+			}}
+		/>
 		<LabeledInput
 			name="name"
 			type="text"
@@ -34,6 +93,6 @@
 			bind:value={$form.bio}
 			{...$constraints.bio}
 		/>
-		<button class="btn mt-10 w-full variant-filled-primary">Update Profile</button>
+		<ButtonWithSpinner btnClasses="mt-5" {isLoading}>Update Profile</ButtonWithSpinner>
 	</div>
 </form>
