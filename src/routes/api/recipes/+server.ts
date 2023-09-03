f596c81c5500fa61e75937678fe42ea90d798275
Jonathan Juayong <jonathan.juayong@gmail.com>
2023-09-03 15:23:42 +1000

    feat: replace db query to allow the endpoint to be searchable by author id

--- src/routes/api/recipes/+server.ts
+++ src/routes/api/recipes/+server.ts
@@ -1,16 +1,23 @@
-import { deleteRecipe, searchRecipesByNamePaginated } from '$lib/server/db';
+import { deleteRecipe, getRecipesByNameByUserPaginated } from '$lib/server/db';
 import { json, redirect } from '@sveltejs/kit';
 
 export const POST = async ({ request }) => {
-	const { searchQuery, lastSeenId, resultsPerPage } = (await request.json()) as {
+	const { searchQuery, lastSeenId, resultsPerPage, authorId } = (await request.json()) as {
 		searchQuery: string;
 		lastSeenId: number;
 		resultsPerPage: number;
+		authorId?: string | undefined;
 	};
 
 	// try to look 1 row ahead to check if data returned is the last batch of data,
 	// hence the + 1 on resultsPerPage
-	const recipes = await searchRecipesByNamePaginated(searchQuery, lastSeenId, resultsPerPage + 1);
+
+	const recipes = await getRecipesByNameByUserPaginated(
+		authorId ?? '',
+		searchQuery,
+		resultsPerPage + 1,
+		lastSeenId
+	);
 
 	// disregard the extra row fetched as it is only used to check if there are still data to be fetched
 	const response = recipes.slice(0, resultsPerPage);
