ede1a36488ca4312624f8f100c430e4307e23e44
Jonathan Juayong <jonathan.juayong@gmail.com>
2023-09-05 16:20:09 +1000

    feat: iterate over links array to reduce code duplication

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -1,21 +1,32 @@
 <script lang="ts">
 	import PotIcon from '~icons/mdi/pot-steam';
 	import AccountIcon from '~icons/mdi/account-circle';
+	import HeartIconFilled from '~icons/mdi/cards-heart';
+
+	const links = [
+		{
+			href: '/app/myrecipes',
+			label: 'My Recipes',
+			Icon: PotIcon
+		},
+		{
+			href: '/app/profile',
+			label: 'My Profile',
+			Icon: AccountIcon
+		},
+		{
+			href: '/app/likes',
+			label: 'My Likes',
+			Icon: HeartIconFilled
+		}
+	];
 </script>
 
 <div class="grid sm:grid-cols-2 gap-8">
-	<a
-		class="card aspect-video grid gap-4 place-content-center place-items-center"
-		href="/app/myrecipes"
-	>
-		<PotIcon class="w-14 h-14" />
-		<h2 class="h3">My Recipes</h2>
-	</a>
-	<a
-		class="card aspect-video grid gap-4 place-content-center place-items-center"
-		href="/app/profile"
-	>
-		<AccountIcon class="w-14 h-14" />
-		<h2 class="h3">My Profile</h2>
-	</a>
+	{#each links as { href, label, Icon }}
+		<a class="card aspect-video grid gap-4 place-content-center place-items-center" {href}>
+			<Icon class="w-14 h-14" />
+			<h2 class="h3">{label}</h2>
+		</a>
+	{/each}
 </div>
